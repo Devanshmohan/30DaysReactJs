62bd960134b7bb2ffcab47b38e7581480bf87a12
Devanshmohan <mdevansh756@gmail.com>
2025-04-10 11:51:32 +0530

    Initial commit

---
+++ src/components/One.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const One = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='font-bold text-2xl'>Hello World!</h1>
+    </div>
+  )
+}
+
+export default One
