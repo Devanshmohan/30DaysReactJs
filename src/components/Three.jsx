d7587db692c05f1557895340851f1074e4758d78
Devanshmohan <147362750+Devanshmohan@users.noreply.github.com>
2025-04-12 21:13:18 +0530

    Add files via upload

---
+++ src/components/Three.jsx
@@ -0,0 +1,23 @@
+// A form that takes user input and displays it in real time
+
+import React, { useState } from 'react'
+
+const Three = () => {
+    const [data,setData] = useState("");
+    const dataChange = (e) =>{
+        setData(e.target.value);
+    }
+  return (
+    
+    <div className='flex justify-center items-center h-screen bg-[#081b29]'>
+        <div className='flex flex-col items-center border-2  max-w-md w-full p-8 bg-yellow-50 rounded-2xl'>
+      <input className='border-black border-2 p-4 rounded-2xl mb-4' onChange={dataChange} placeholder='Enter text' type="text" />
+
+    <p className='mx-2 break-words w-full text-2xl'>User Input : {data}</p>
+    </div>
+    </div>
+          
+  )
+}
+
+export default Three
