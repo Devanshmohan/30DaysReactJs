a6ebc21e61bfd822c40876641e07afd8f9fd5dc4
Devanshmohan <mdevansh756@gmail.com>
2025-04-12 21:08:41 +0530

    Uploaded third component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import One from './components/One'
 import Two from './components/Two'
+import Three from './components/Three'
 
 const App = () => {
   return (
     <div>
      {/* <One/> */}
-     <Two />
+     {/* <Two /> */}
+     <Three />
     </div>
   )
 }
--- src/components/Two.jsx
+++ src/components/Two.jsx
@@ -10,9 +10,12 @@ const Two = () => {
     <div className='flex justify-center items-center h-screen'>
       <button 
       disabled = {value===0}
-      className={ `rounded-2xl text-lg font-bold mx-2  ${value ===0 ? 'bg-gray-400 px-6 py-2 cursor-not-allowed' : 'bg-red-500 px-6 py-2 cursor-pointer'}`}  onClick={()=>setValue(value-1)}>-</button>
+      className={ `rounded-2xl text-lg font-bold mx-2 
+       ${value ===0 ? 'bg-gray-400 px-6 py-2 cursor-not-allowed' : 
+       'bg-red-500 px-6 py-2 cursor-pointer'}`}  onClick={()=>setValue(value-1)}>-</button>
       {value}
-      <button className='rounded-2xl bg-blue-500 px-6 py-2 text-lg font-bold mx-2' onClick={()=>setValue(value+1)}>+</button>
+      <button className='rounded-2xl bg-blue-500 px-6 py-2 text-lg font-bold mx-2'
+       onClick={()=>setValue(value+1)}>+</button>
     </div>
   )
 }
